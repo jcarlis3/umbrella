d023664a30fab874894b04b44b12aecff95fe87f
Jason Carlisle <jason.d.carlisle@gmail.com>
2015-06-24 13:25:34 -0600

    Initial commit of help docs for data
    Used a single .R file and roxygen2 to document all three datasets.

---
+++ R/datasets.R
@@ -0,0 +1,29 @@
+#' demo.sdm
+#' An example species distribution model (SDM).
+#' The SDM is coded such that 1 = suitable habitat and 0 = not suitable habitat.
+#' @docType data
+#' @name demo.sdm
+#' @usage data(demo.sdm)
+#' @format Formal class RasterLayer
+#' @author Jason D. Carlisle, University of Wyoming, <jason.d.carlisle@@gmail.com>
+NULL
+
+
+#' demo.rsv
+#' An example reserve (e.g., a protected area created for an umbrella species).
+#' @docType data
+#' @name demo.rsv
+#' @usage data(demo.rsv)
+#' @format Formal class SpatialPolygons
+#' @author Jason D. Carlisle, University of Wyoming, <jason.d.carlisle@@gmail.com>
+NULL
+
+
+#' demo.msk
+#' An example availability mask (i.e., the polygon within which simulated reserves can be placed).
+#' @docType data
+#' @name demo.msk
+#' @usage data(demo.msk)
+#' @format Formal class SpatialPolygons
+#' @author Jason D. Carlisle, University of Wyoming, <jason.d.carlisle@@gmail.com>
+NULL
