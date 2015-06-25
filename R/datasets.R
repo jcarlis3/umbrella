1cb4ad37bf259693fe9a61031dd4cc0c5723be2c
Jason Carlisle <jason.d.carlisle@gmail.com>
2015-06-24 22:23:31 -0600

    Fixes to dataset documentation
    Formatting fixes.

--- R/datasets.R
+++ R/datasets.R
@@ -1,5 +1,7 @@
 #' demo.sdm
-#' An example species distribution model (SDM).
+#'
+#' An example species distribution model (SDM)
+#'
 #' The SDM is coded such that 1 = suitable habitat and 0 = not suitable habitat.
 #' @docType data
 #' @name demo.sdm
@@ -10,7 +12,9 @@ NULL
 
 
 #' demo.rsv
+#'
 #' An example reserve (e.g., a protected area created for an umbrella species).
+#'
 #' @docType data
 #' @name demo.rsv
 #' @usage data(demo.rsv)
@@ -20,7 +24,9 @@ NULL
 
 
 #' demo.msk
+#'
 #' An example availability mask (i.e., the polygon within which simulated reserves can be placed).
+#'
 #' @docType data
 #' @name demo.msk
 #' @usage data(demo.msk)
