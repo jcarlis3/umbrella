f8d0c920019ed905a01bd82477fceddae18ab97f
Jason Carlisle <jason.d.carlisle@gmail.com>
2015-06-26 15:50:16 -0600

    Update to help docs
    Renamed to help.R, now documents the package itself and the three
    datasets.

--- R/help.R
+++ R/help.R
@@ -1,3 +1,22 @@
+# This document includes the help documentation for non-functions (i.e., the package itself and the example datasets).
+# The following code is interpreted by roxygen2 into separate .Rd files:
+
+
+#' This package contains functions that aid in evaluating the performance of a conservation reserve in protecting the
+#' suitable habitat of species of conservation interest.
+#'
+#' \bold{version 0.1}:  Contains two functions (\code{sumRaster} and \code{simReserve}) and three example
+#' datasets (\code{demo.sdm}, \code{demo.rsv}, and \code{demo.msk}).  Demo of package submitted as supplement to
+#' published manuscript.
+#'
+#' Bug reports or suggestions for the umbrella package should be directed to <jason.d.carlisle@@gmail.com>
+#' @title umbrella:  Evaluating Conservation Reserves
+#' @docType package
+#' @name umbrella
+#' @author Jason D. Carlisle, University of Wyoming, <jason.d.carlisle@@gmail.com>
+NULL
+
+
 #' demo.sdm
 #'
 #' An example species distribution model (SDM)
